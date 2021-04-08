d83141abda9faed8315293fb8c2a76f828da3887
Atheana0929 <81278000+Atheana0929@users.noreply.github.com>
2021-04-08 21:01:13 +0800

    Add files via upload

--- 最大概似定理.R
+++ 最大概似定理.R
@@ -5,6 +5,8 @@
 # Step2:將概似函數取ln 
 # Step3:解釋 -> 發生這個x1這個值大概的機率為
 
+### 第一堂課 ###
+
 rm(list = ls())
 
 logL <- function(pm, x) {
@@ -32,10 +34,33 @@ logL <- function(pm, x) {
 ##先將data隨意輸入
 data <-  rnorm(10000, mean = 0.3, sd= 0.2)    # rnorm拆解 { [r]=random , [norm]=normal }
 
-estimate <-  optim(par = c(0,1),
-                   fn = logL,
+estimate <-  optim(par    = c(0,1),
+                   fn     = logL,
                    method = "L-BFGS-B",
-                   lower = c(-1,2**(-52)),   #設定局部極小值
-                   upper = c(1,1),           #設定局部極大值 
+                   lower  = c(-1,2**(-52)),   #設定局部極小值
+                   upper  = c(1,1),           #設定局部極大值 
                    x = data)          
+
+
+pm_1 <-  estimation$par
+
+
+### 第二堂課 ###
+# 
+# 效用函數: Max U(x,y) = x^(0.2)*Y^(0.6)
+# 限制式st: 2X+5Y =200
+
+
+u_fun  <- function(x) {
+  return(x^0.2*(40-0.4*x)^0.6)
+  
+}
+
+
+result <-  optim(par    = 5,
+                 fn     = u_fun,
+                 method = "L-BFGS-B",
+                 lower  = 2^(-52),
+                 upper  = 100)
+                 
 
